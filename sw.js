a9e644d0ab19ae0921e34f1bee31225d2b703178
sklgim87er-ship-it <sklgim87er@gmail.com>
2026-02-11 16:40:11 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = "no-entry-v3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./magisystem.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request))
+  );
+});
